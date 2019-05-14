ffaef27887fd20c4f9c3b2a83b7bf773ac41d2b0
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-14 13:47:11 +0100

    Chore: Add the capture syntax so we bounce from the document to other nested elkements to the target directly

--- js/index.js
+++ js/index.js
@@ -3,9 +3,18 @@ const navigationItems = document.querySelectorAll('.nav-link');
 const navContainer = document.querySelector('nav');
 const mainNavigationContainer = document.querySelector('.nav-container');
 
+navContainer.addEventListener('click', () => {
+    console.log('you are in the header');
+}, {capture: true});
+
+mainNavigationContainer.addEventListener('click', () => {
+    console.log('you are in the main nav container');
+}, {capture: true});
+
 // better way of appying preventDefault() to all navigation items
 navigationItems.forEach((item) => {
     item.addEventListener('click', (e) => {
         e.preventDefault();
+        console.log('you are in an individual nav-link')
     });
-});
+}, {capture: true});
