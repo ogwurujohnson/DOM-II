17d4c351f78640f9710e7178050370ab350a17a2
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-14 13:42:29 +0100

    Fix: Add prevent default to all nav items

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 // Your code goes here
-const navigationItems = document.querySelector('.nav-link');
+const navigationItems = document.querySelectorAll('.nav-link');
+const navContainer = document.querySelector('nav');
+const mainNavigationContainer = document.querySelector('.nav-container');
 
-
-navigationItems.addEventListener('click', (e) => {
-    e.preventDefault();
+// better way of appying preventDefault() to all navigation items
+navigationItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+    });
 });
-
